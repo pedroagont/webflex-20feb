194e6415c8a3e6219988ddc114e14c3f976116d1
Pedro A. González <pgonzalez@post.com>
2023-02-23 13:48:29 -0600

    Solved the sum code challenge

--- m01w01/example.js
+++ m01w01/example.js
@@ -1,5 +1,24 @@
 // Problem:
 // Write a node program that takes in an unlimited number of command line arguments
-// and prints out the sum of them. 
-// If any argument is not a whole number, skip it. 
+// and prints out the sum of them.
+// If any argument is not a whole number, skip it.
 // Do not support negative numbers though.
+
+// console.log('Hi from terminal!');
+// console.log(process.argv);
+
+const inputValues = process.argv.slice(2);
+
+let sum = 0;
+
+for (const item of inputValues) {
+  //   console.log(typeof Number(item));
+
+  const converted = Number(item);
+
+  if (Number.isInteger(converted) && converted > 0) {
+    sum = sum + converted;
+  }
+}
+
+console.log(sum);
