9aa7840e9c694a08684879ec2f40c3612bac1a9d
Pedro A. González <pgonzalez@post.com>
2023-02-23 13:12:40 -0600

    add example file for exercise

---
+++ m01w01/example.js
@@ -0,0 +1,5 @@
+// Problem:
+// Write a node program that takes in an unlimited number of command line arguments
+// and prints out the sum of them. 
+// If any argument is not a whole number, skip it. 
+// Do not support negative numbers though.
