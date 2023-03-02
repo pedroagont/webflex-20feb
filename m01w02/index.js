c528f46f14ec8618e6ca747596108750a910c995
Pedro A. González <pgonzalez@post.com>
2023-03-02 13:58:44 -0600

    added map and filter functions + other examples

--- m01w02/index.js
+++ m01w02/index.js
@@ -11,6 +11,7 @@ const sayHi = function () {
   console.log('Hi 👋');
 };
 
+// function calling vs function passing
 const otherThing = sayHi;
 
 otherThing();
@@ -30,17 +31,49 @@ const addTwo = function (num) {
 
 runMyFunction(sayHello);
 
+// functions that receive arguments as callbacks
 runMyFunction(function () {
   addTwo(10);
 });
 
+// arrow functions
 runMyFunction(() => addTwo(10));
 
+// Higher order functions and callbacks
+const welcomeUserHigherOrderFn = (username, cb) => cb(username);
+
+const profileCompleteCb = function (username) {
+  console.log(`Welcome ${username}, your profile is complete!`);
+};
+
+const profileMissingDataCb = function (username) {
+  console.log(`Welcome ${username}, your profile is missing data!`);
+};
+
+welcomeUserHigherOrderFn('Aziz', profileCompleteCb);
+welcomeUserHigherOrderFn('Nicol', profileMissingDataCb);
+
+const mathResult = (num1, num2, operation) => operation(num1, num2);
+
+const mySum = mathResult(2, 2, (num1, num2) => num1 + num2);
+console.log(mySum);
+
+const mySub = mathResult(2, 2, (num1, num2) => num1 - num2);
+console.log(mySub);
+
+const myMult = mathResult(2, 2, (num1, num2) => num1 * num2);
+console.log(myMult);
+
+const myDiv = mathResult(2, 2, (num1, num2) => num1 / num2);
+console.log(myDiv);
+
+// implementing callbacks for arrays iteration
 const fruits = ['apple', 'orange', 'kiwi'];
 for (const fruit of fruits) {
   console.log(fruit);
 }
 
+// logEach
 const logEach = function (arr) {
   for (const item of arr) {
     console.log(item);
@@ -52,6 +85,7 @@ logEach(fruits);
 const numbers = [1, 2, 3];
 logEach(numbers);
 
+// forEach
 const forEach = function (arr, action) {
   for (const item of arr) {
     action(item);
@@ -65,3 +99,47 @@ forEach(fruits, function (fruit) {
 forEach(numbers, function (num) {
   console.log(num + 2);
 });
+
+// map
+const map = (arr, cb) => {
+  const result = [];
+
+  for (const item of arr) {
+    result.push(cb(item));
+  }
+
+  return result;
+};
+
+const original = [1, 2, 3, 4];
+const mappedPlus2 = map(original, (num) => num + 100);
+const mappedTimes2 = map(original, (num) => num * 2);
+console.log(original);
+console.log(mappedPlus2);
+console.log(mappedTimes2);
+
+const happyFruits = map(fruits, (f) => 'happy ' + f);
+console.log(happyFruits);
+
+logEach(happyFruits);
+
+// filter
+const filter = (arr, cb) => {
+  const result = [];
+
+  for (const item of arr) {
+    if (cb(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+const ages = [32, 33, 16, 40, 12];
+
+const adults = filter(ages, (age) => age >= 18);
+console.log(adults);
+
+const teens = filter(ages, (age) => age < 18);
+console.log(teens);
