cb47f8bdcf906c61b45ad58d5416d4802cc010ed
Pedro A. González <pgonzalez@post.com>
2023-04-07 14:47:44 -0500

    Add extra notes and video link

--- m03w07/server.js
+++ m03w07/server.js
@@ -4,9 +4,8 @@ const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 // var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
-const app = express();
-const port = 3000;
 
+// Simulates a DB
 const db = {
   notes: {
     1: {
@@ -36,25 +35,36 @@ const db = {
   },
 };
 
-// ------------------ MIDDLEWARE
+// ------------------ SETUP / MIDDLEWARE
+const app = express();
+const port = 3000;
+
+// Custom middleware
 // app.use((req, res, next) => {
 //   console.log(req.url, req.method);
 //   next();
 // });
+
+// external middleware like morgan to log every request to our server
 app.use(morgan('dev'));
 
+// sets the template engine as html with embbedded js (views/*.ejs)
 app.set('view engine', 'ejs');
 
+// allow our server to parse/decode data through the use of a body (req.body)
 app.use(express.urlencoded({ extended: false }));
 
+// cookie-parser parses a cookies object in the request object (req.cookies)
 // app.use(cookieParser());
+
+// cookie-session parses a session object in the request object (req.session)
 app.use(
   cookieSession({
     name: 'session',
     keys: ['secret', 'keys'],
     // Cookie Options
     // maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    maxAge: 10 * 60 * 1000, // 24 hours
+    maxAge: 10 * 60 * 1000, // 10 min
   })
 );
 
@@ -64,180 +74,241 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test/:id', (req, res) => {
-  console.log('req.params', req.params);
-  console.log('req.query', req.query);
+  console.log('req.params', req.params); // url.com/users/123/products/456
+  console.log('req.query', req.query); // url.com/cities/cancun?min=100&max=300
   res.send('Test route!');
 });
 
-// RENDERING ROUTES
-// NOTES
+// NOTES RENDERING ROUTES - INTERACTIONS WITH THE USER
+// NOTES (LIST, NEW, SHOW)
 // NOTES LIST
 app.get('/notes', (req, res) => {
+  // cookie/ user logged in validation
   const { user_id } = req.session;
   if (!user_id) {
     return res.status(400).send('User is not logged in');
   }
 
+  // populate template variables
   const templateVars = {
     notes: db.notes,
   };
+
+  // render template with variables
   res.render('notes-list', templateVars);
 });
 
 // NOTES NEW
 app.get('/notes/new', (req, res) => {
+  // cookie/ user logged in validation
   const { user_id } = req.session;
   if (!user_id) {
     return res.status(400).send('User is not logged in');
   }
 
+  // render template
   res.render('notes-new');
 });
 
 // NOTES SHOW
 app.get('/notes/:id', (req, res) => {
+  // cookie/ user logged in validation
   const { user_id } = req.session;
   if (!user_id) {
     return res.status(400).send('User is not logged in');
   }
 
-  const { id } = req.params;
-  const note = db.notes[id];
+  // Refer to the note and validate it exists
+  const note = db.notes[req.params.id];
   if (!note) {
     res.status(404).send('Note not found');
   }
 
+  // populate template variables and render
   const templateVars = { note };
-
   res.render('notes-show', templateVars);
 });
 
-// RENDERING ROUTES AUTH
+// AUTH RENDERING ROUTES - INTERACTIONS WITH THE USER
 // AUTH REGISTER
 app.get('/register', (req, res) => {
+  // render register form
   res.render('register');
 });
 
 // AUTH LOGIN
 app.get('/login', (req, res) => {
+  // render login form
   res.render('login');
 });
 
-// CRUD API NOTES
-// CREATE - POST
+// REST API CRUD NOTES - NOT INTERACTING WITH THE USER, JUST DATA HANDLERS
+// CREATE NOTE API - POST
 app.post('/api/notes', (req, res) => {
+  // cookie/ user logged in validation
   const { user_id } = req.session;
   if (!user_id) {
     return res.status(400).send('User is not logged in');
   }
 
+  // body properties validation
   const { content } = req.body;
+  if (!content) {
+    return res.status(400).send('Provide content to create a note');
+  }
+
+  // creates an id and stores new note object in database
   const id = Math.floor(Math.random() * 100);
   db.notes[id] = {
     id,
     content,
   };
+
+  // print database to see results
   console.log(db.notes);
 
+  // redirect to main notes page to see new created note
   res.redirect('/notes');
 });
 
-// READ ALL - GET
+// READ ALL NOTES API - GET
 app.get('/api/notes', (req, res) => {
+  // all notes in json
   res.send(db.notes);
 });
 
-// READ ONE - GET
+// READ ONE NOTE API - GET
 app.get('/api/notes/:id', (req, res) => {
-  const { id } = req.params;
-  res.send(db.notes[id]);
+  // Refer to the note and validate it exists
+  const note = db.notes[req.params.id];
+  if (!note) {
+    res.status(404).send('Note not found');
+  }
+
+  // one note in json
+  res.send(note);
 });
 
-// UPDATE - POST/PUT
+// UPDATE NOTE API - POST/PUT
 app.post('/api/notes/:id/edit', (req, res) => {
+  // cookie/ user logged in validation
   const { user_id } = req.session;
   if (!user_id) {
     return res.status(400).send('User is not logged in');
   }
 
+  // body properties validation
   const { content } = req.body;
-  const { id } = req.params;
-  db.notes[id] = {
-    id,
-    content,
-  };
+  if (!content) {
+    return res.status(400).send('Provide content to create a note');
+  }
+
+  // Refer to the note and validate it exists
+  const note = db.notes[req.params.id];
+  if (!note) {
+    res.status(404).send('Note not found');
+  }
+
+  // after validating all steps, update the notes content
+  db.notes[req.params.id].content = content;
+
+  // print database to see results
+  console.log(db.notes);
+
+  // redirect to main notes page
+  res.redirect('/notes');
+});
+
+// DELETE NOTE API - POST/DELETE
+app.post('/api/notes/:id/delete', (req, res) => {
+  // cookie/ user logged in validation
+  const { user_id } = req.session;
+  if (!user_id) {
+    return res.status(400).send('User is not logged in');
+  }
+
+  // Refer to the note and validate it exists
+  const note = db.notes[req.params.id];
+  if (!note) {
+    res.status(404).send('Note not found');
+  }
+
+  // after validating all steps, delete the note
+  delete db.notes[req.params.id];
+
+  // print database to see results
   console.log(db.notes);
 
   res.redirect('/notes');
 });
 
-// DELETE - POST/DEL
-app.post('/api/notes/:id/delete', (req, res) => {
-  const { user_id } = req.session;
-  if (!user_id) {
-    return res.status(400).send('User is not logged in');
-  }
-
-  const { id } = req.params;
-  delete db.notes[id];
-  res.redirect('/notes');
-});
-
-// AUTH API
-// AUTH REGISTER
+// REST API AUTH
+// AUTH REGISTER API
 app.post('/api/register', (req, res) => {
+  // body properties validation
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).send('Provide email and password to register');
   }
 
+  // Create id and hash pasword
   const id = Math.floor(Math.random() * 100);
   const hashedPassword = bcrypt.hashSync(password, 8);
 
+  // Store new user object in database with new id
   db.users[id] = {
     email,
     password: hashedPassword,
   };
+
+  // print db to see results
   console.log(db.users);
   res.redirect('/login');
 });
 
-// AUTH LOGIN
+// AUTH LOGIN API
 app.post('/api/login', (req, res) => {
-  console.log(req.body);
+  // body properties validation
   const { email, password } = req.body;
-
-  let userObject = null;
-  for (const key in db.users) {
-    if (db.users[key].email === email) {
-      userObject = db.users[key];
-    }
+  if (!email || !password) {
+    return res.status(400).send('Provide email and password to register');
   }
 
-  if (!userObject) {
+  // validates user exists in database
+  let user = null;
+  for (const key in db.users) {
+    if (db.users[key].email === email) {
+      user = db.users[key];
+    }
+  }
+  if (!user) {
     return res.status(401).send('Invalid credentials');
   }
 
-  //   const passwordsMatch = userObject.password === password;
-  const passwordsMatch = bcrypt.compareSync(password, userObject.password);
+  // password validation
+  //   const passwordsMatch = user.password === password;
+  const passwordsMatch = bcrypt.compareSync(password, user.password);
   if (!passwordsMatch) {
     return res.status(401).send('Invalid credentials');
   }
 
+  // after all validation steps we set the cookie
   //   res.cookie('user_id', email);
   req.session.user_id = email;
 
-  console.log(db.users);
   res.redirect('/notes');
 });
 
-// LOGOUT
+// AUTH LOGOUT API
 app.post('/api/logout', (req, res) => {
+  // remove cookie to logout current user
   //   res.clearCookie('user_id');
   req.session = null;
+
   res.redirect('/login');
 });
 
+// Catch all route
 app.use((req, res) => {
   res.status(404).send('Not found!');
 });
